ed0053de3377208e85a3fd4dfe54de2238b2f997
David C. Lonie <david.lonie@kitware.com>
2013-04-04 12:50:59 -0400

    Test if other RPC server is alive and act appropriately.
    Ping the server, and if a reply is received, do nothing. If no reply
    is received, prompt to replace the server.
    
    Change-Id: Ic0596356951cae363dfac33cc3eddb38aa87be59

--- avogadro/rpclistener.cpp
+++ avogadro/rpclistener.cpp
@@ -16,7 +16,8 @@
 
 #include "rpclistener.h"
 
-#include <QApplication>
+#include <QtGui/QApplication>
+#include <QtGui/QInputDialog>
 
 #include "qjsonvalue.h"
 #include "qjsonobject.h"
@@ -26,18 +27,24 @@
 
 #include "mainwindow.h"
 
+#include <molequeue/client/jsonrpcclient.h>
 #include <molequeue/transport/jsonrpc.h>
 #include <molequeue/transport/localsocketconnectionlistener.h>
 
 namespace Avogadro {
 
 RpcListener::RpcListener(QObject *parent_)
-  : QObject(parent_)
+  : QObject(parent_),
+    m_pingSuccessful(false)
 {
   m_rpc = new MoleQueue::JsonRpc(this);
 
   m_connectionListener =
     new MoleQueue::LocalSocketConnectionListener(this, "avogadro");
+
+  connect(m_connectionListener,
+          SIGNAL(connectionError(MoleQueue::ConnectionListener::Error,QString)),
+          SLOT(connectionError(MoleQueue::ConnectionListener::Error,QString)));
 
   m_rpc->addConnectionListener(m_connectionListener);
 
@@ -72,8 +79,49 @@ void RpcListener::start()
 void RpcListener::connectionError(MoleQueue::ConnectionListener::Error error,
                                   const QString &message)
 {
-  Q_UNUSED(error)
-  qDebug() << "RpcListener connection error: " << message;
+  qDebug() << "Error starting RPC server:" << message;
+  if (error == MoleQueue::ConnectionListener::AddressInUseError) {
+    // Try to ping the existing server to see if it is alive:
+    if (MoleQueue::JsonRpcClient::pingServer(
+          m_connectionListener->connectionString(), 200)) {
+      qDebug() << "Other server is alive. Not starting new instance.";
+      return;
+    }
+    else {
+      QStringList items;
+      items << tr("Replace the dead server with a new instance.");
+      items << tr("Start without RPC capabilities.");
+      QString title(tr("Error starting RPC server:"));
+      QString label(
+            tr("An error occurred while starting Avogadro's RPC listener. "
+               "This may be happen for a\nnumber of reasons:\n\t"
+               "A previous instance of Avogadro may have crashed.\n\t"
+               "A running Avogadro instance was too busy to respond.\n\n"
+               "If no other Avogadro instance is running on this machine, it "
+               "is safe to replace the dead\nserver. "
+               "Otherwise, this instance of avogadro may be started without "
+               "RPC capabilities\n(this will prevent RPC enabled applications "
+               "from communicating with Avogadro)."));
+      bool ok(false);
+      QString item(QInputDialog::getItem(NULL, title, label, items, 0, false,
+                                         &ok));
+
+      if (ok && item == items.first()) {
+        qDebug() << "Starting new server.";
+        m_connectionListener->stop(true);
+        m_connectionListener->start();
+      }
+      else {
+        qDebug() << "Starting without RPC capabilities.";
+      }
+    }
+  }
+}
+
+void RpcListener::receivePing(const QJsonObject &response)
+{
+  m_pingSuccessful = response.value("result").isString()
+     && response.value("result").toString() == QString("pong");
 }
 
 void RpcListener::messageReceived(const MoleQueue::Message &message)
