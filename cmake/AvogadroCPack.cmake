004d1b7845d878f56d7858b73e82e8956d5b8350
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-04-05 01:18:14 -0400

    Customize CPack a little, get NSIS working
    We want CPack to use our version, rather than the default 0.1.1, add
    the license file and icon setup along with some useful links once
    everything is installed.
    
    Change-Id: Icbcd16d7a63e967b84f8c981289bbad86a4a37e7

---
+++ cmake/AvogadroCPack.cmake
@@ -0,0 +1,17 @@
+set(CPACK_PACKAGE_NAME "Avogadro2")
+set(CPACK_PACKAGE_VERSION_MAJOR ${AvogadroApp_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${AvogadroApp_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${AvogadroApp_VERSION_PATCH})
+set(CPACK_PACKAGE_VERSION ${AvogadroApp_VERSION})
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "Avogadro2")
+set(CPACK_RESOURCE_FILE_LICENSE "${AvogadroApp_SOURCE_DIR}/COPYING")
+
+set(CPACK_PACKAGE_EXECUTABLES "avogadro" "Avogadro2")
+set(CPACK_CREATE_DESKTOP_LINKS "avogadro")
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/AvogadroCPackOptions.cmake.in"
+  "${AvogadroApp_BINARY_DIR}/AvogadroCPackOptions.cmake" @ONLY)
+set(CPACK_PROJECT_CONFIG_FILE
+  "${AvogadroApp_BINARY_DIR}/AvogadroCPackOptions.cmake")
+
+include(CPack)
