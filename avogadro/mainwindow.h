bd87f74bc85c5d26821eff42e3776a46c8263333
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-04-14 13:14:25 -0400

    Updated and expanded the MainWindow documentation
    Change-Id: Ieb7defcb89b5d7084a9960339e6c2773b56db00a

--- avogadro/mainwindow.h
+++ avogadro/mainwindow.h
@@ -2,7 +2,7 @@
 
   This source file is part of the Avogadro project.
 
-  Copyright 2012 Kitware, Inc.
+  Copyright 2012-2013 Kitware, Inc.
 
   This source code is released under the New BSD License, (the "License").
 
@@ -47,6 +47,14 @@ class ExtensionPlugin;
 class Molecule;
 }
 
+/**
+ * @class MainWindow
+ * @author Marcus D. Hanwell
+ *
+ * The MainWindow class for the Avogadro application. Takes care of initializing
+ * the application and overall layout.
+ */
+
 class MainWindow : public QMainWindow
 {
 Q_OBJECT
@@ -64,31 +72,84 @@ public slots:
 public:
   QtGui::Molecule * molecule() { return m_molecule; }
 
+  /**
+   * Write out all application settings, normally done as part of the
+   * application close event.
+   */
   void writeSettings();
+
+  /**
+   * Read in all settings, and initialize our application based on the stored
+   * settings.
+   */
   void readSettings();
 
 signals:
+  /**
+   * Emitted when the active molecule in the application has changed.
+   */
   void moleculeChanged(QtGui::Molecule *molecue);
 
 protected:
   void closeEvent(QCloseEvent *event);
 
 protected slots:
+  /**
+   * Slot provided for extensions to indicate a molecule is ready to be read in.
+   * This slot will then pass a molecule to the extension for the data to be
+   * read in to.
+   */
   void moleculeReady(int number);
+
+  /**
+   * Create a new molecule and make it the active molecule.
+   */
   void newMolecule();
+
+  /**
+   * Prompt for a file location, and attempt to open the specified file using
+   * our native readers.
+   */
   void openFile();
+
+  /**
+   * Import a file, using the full selection of formats capable of reading.
+   */
   void importFile();
-  /// If specified, use the FileFormat @a reader to save the file. This method
-  /// takes ownership of @a reader and will delete it before returning. If not
-  /// specified, a reader will be selected based on fileName's extension.
+
+  /**
+   * If specified, use the FileFormat @a reader to save the file. This method
+   * takes ownership of @a reader and will delete it before returning. If not
+   * specified, a reader will be selected based on fileName's extension.
+   */
   void openFile(const QString &fileName, Io::FileFormat *reader = NULL);
+
+  /**
+   * Open file in the recent files list.
+   */
   void openRecentFile();
+
+  /**
+   * Update the list of recent files.
+   */
   void updateRecentFiles();
+
+  /**
+   * Prompt for a file location, and attempt to save the active molecule to the
+   * specified location.
+   */
   void saveFile();
+
+  /**
+   * Export a file, using the full selection of formats capable of writing.
+   */
   void exportFile();
-  /// If specified, use the FileFormat @a writer to save the file. This method
-  /// takes ownership of @a writer and will delete it before returning. If not
-  /// specified, a writer will be selected based on fileName's extension.
+
+  /**
+   * If specified, use the FileFormat @a writer to save the file. This method
+   * takes ownership of @a writer and will delete it before returning. If not
+   * specified, a writer will be selected based on fileName's extension.
+   */
   void saveFile(const QString &fileName, Io::FileFormat *writer = NULL);
 
 #ifdef QTTESTING
@@ -137,9 +198,24 @@ private:
   bool m_testExit;
 #endif
 
+  /**
+   * Build the main menu, delayed until all plugins have registered actions.
+   */
   void buildMenu();
+
+  /**
+   * Add the menu entries for the extension passed in.
+   */
   void buildMenu(QtGui::ExtensionPlugin *extension);
+
+  /**
+   * Initialize the tool plugins.
+   */
   void buildTools(QList<Avogadro::QtGui::ToolPlugin *> toolList);
+
+  /**
+   * Convenience function to generate a filter string for the supplied formats.
+   */
   QString generateFilterString(
       const std::vector<const Io::FileFormat *> &formats,
       bool addAllEntry = true);
