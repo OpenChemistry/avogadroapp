d950cf126dd74cbab58591f0bb5c9f5dc0f4b92a
Geoff Hutchison <geoff.hutchison@gmail.com>
2021-09-08 15:37:23 -0400

    Try flatpak build with each push and pull request
    Once this works, we'll switch to each push to master
    
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

---
+++ scripts/flatpak-update.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# simple script to revise the flatpak manifest
+# to allow git builds
+
+import yaml
+
+with open("org.openchemistry.Avogadro2.yaml") as stream:
+    # use safe_load as it cannot execute arbitrary code
+    document = yaml.safe_load(stream)
+    for module in document["modules"]:
+        if type(module) is not dict:
+            continue
+
+        if module["name"] == "avogadrolibs":
+            for sources in module["sources"]:
+                print(sources)
+                if type(sources) is not dict:
+                    continue
+                if sources["url"] == "https://github.com/OpenChemistry/avogadrolibs":
+                    sources.pop("tag", None)
+                    sources.pop("commit", None)
+        elif module["name"] == "Avogadro2":
+            for sources in module["sources"]:
+                print(sources)
+                if type(sources) is not dict or "url" not in sources:
+                    continue
+                if sources["url"] == "https://github.com/OpenChemistry/avogadroapp":
+                    sources.pop("tag", None)
+                    sources.pop("commit", None)
+
+with open("org.openchemistry.Avogadro2.yaml", "w") as stream:
+    yaml.dump(document, stream)
