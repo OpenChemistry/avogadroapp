686d94e687e234e266f3e5e0d1fdfbbd8d9da889
aditya <adityaomar33@gmail.com>
2023-10-03 21:07:42 +0530

    Future class not needed in python3, revc_msg bug fix, The file is now completely functional
    Signed-off-by: aditya <adityaomar33@gmail.com>
    Signed-off-by: Omar <adityaomar33@gmail.com>

--- scripts/avogadro-remote.py
+++ scripts/avogadro-remote.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python
-
-from __future__ import print_function
-
 import sys
 import json
 import socket
 import struct
 import tempfile
-
 class Connection:
     '''Process a JSON-RPC request'''
-
     def __init__(self, name="avogadro"):
         """
         Connect to the local named pipe
@@ -19,16 +14,14 @@ class Connection:
         """
         # create socket
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
         # connect
         try:
             self.sock.connect(tempfile.gettempdir() + "/" + name)
             # print the connection statement
-            print("reply:" + str(self.__json("recv_msg",None)))
+            print("CONNECTION ESTABLISHED SUCCESSFULLY")
         except Exception as exception:
             print("error while connecting: " + str(exception))
             sys.exit(1)
-
     def __json(self, method, file):
         """
         Send a JSON-RPC request to the named pipe.
@@ -38,15 +31,9 @@ class Connection:
 
         """
         if method == "recv_msg":
-        
             size = 1024
             packet = self.sock.recv(size)
-            return packet[4:]
-            # try:
-            #     return json.loads(msg_packet)
-            # except Exception as exception:
-            #     print("error: " + str(exception))
-            #     return {} 
+            print("reply:" + str(packet[4: ]))
         else:     
             msg = {
                     "jsonrpc": "2.0",
@@ -58,24 +45,24 @@ class Connection:
             size = len(json_msg)
             header = struct.pack(">I", size)
             packet = header + json_msg.encode("ascii")
-            self.sock.send(packet)
-                 
+            self.sock.send(packet)                
     def open_file(self, file):
         """Opens file"""
         # param: file is filename input by the user in string
         method = "openFile"
-        self.__json(method,file)        
-
+        self.__json(method,file)   
+        self.__json("recv_msg",None)     
     def save_graphic(self, file):
         """Save Graphic"""
         method = "saveGraphic"
         self.__json(method,file)
-  
+        self.__json("recv_msg",None)
     def kill(self):
         """To kill the current operation"""
         method = "kill"
         self.__json(method,None)
-  
+        self.__json("recv_msg",None)  
     def close(self):
         '''Close the socket to the named pipe'''
         self.sock.close()
+        print("CONNECTION CLOSED SUCCESSFULLY")
