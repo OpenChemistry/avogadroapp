a78d30d77bd149caab4e935dc790f63d68557095
Geoff Hutchison <geoff.hutchison@gmail.com>
2023-12-28 12:22:31 -0500

    Keep the QCocoaMessageBox code for Qt5
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- avogadro/mainwindow.cpp
+++ avogadro/mainwindow.cpp
@@ -95,7 +95,7 @@
 #include <avogadro/vtk/vtkglwidget.h>
 #endif
 
-#if defined(Q_OS_MAC) && !(QT_VERSION >= 0x050E00)
+#if defined(Q_OS_MAC) && (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
 #include "qcocoamessagebox.h"
 #define MESSAGEBOX QCocoaMessageBox
 #else
