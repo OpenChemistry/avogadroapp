7730a57ee7c0b6e38b0757f9b284ef91e6293a6a
Perminder Singh <127239756+perminder-17@users.noreply.github.com>
2024-09-26 04:48:57 +0530

    fixing

--- avogadro/renderingdialog.h
+++ avogadro/renderingdialog.h
@@ -27,19 +27,19 @@ public:
   ~RenderingDialog() override;
 
   bool aoEnabled();
-  bool dofEnabled();
-  float dofStrength();
-  float aoStrength();
-  float dofPosition();
   float fogPosition();
+  float aoStrength();
   float fogStrength();
   bool fogEnabled();
+  bool dofEnabled();
+  float dofStrength();
+  float dofPosition();
   bool edEnabled();
 
 protected slots:
   void aoEnableCheckBoxChanged(int state);
-  void dofEnableCheckBoxChanged(int state);
   void fogEnableCheckBoxChanged(int state);
+  void dofEnableCheckBoxChanged(int state);
   void saveButtonClicked();
   void closeButtonClicked();
 
