8b0ce8ce9c3db7db315dabd7c0e5db4c6786722f
Geoff Hutchison <geoff.hutchison@gmail.com>
2024-09-29 19:50:23 -0400

    Revert "Port QRegExp to QRegularExpression"

--- avogadro/mainwindow.cpp
+++ avogadro/mainwindow.cpp
@@ -1810,7 +1810,7 @@ void MainWindow::setBackgroundColor()
   GLWidget* glWidget(nullptr);
   if ((glWidget = qobject_cast<GLWidget*>(m_multiViewWidget->activeWidget())))
     scene = &glWidget->renderer().scene();
-  pipeline = &glWidget->renderer().solidPipeline();
+    pipeline = &glWidget->renderer().solidPipeline();
   if (scene) {
     Vector4ub cColor = scene->backgroundColor();
     QColor qtColor(cColor[0], cColor[1], cColor[2], cColor[3]);
@@ -1821,7 +1821,7 @@ void MainWindow::setBackgroundColor()
       cColor[2] = static_cast<unsigned char>(color.blue());
       cColor[3] = static_cast<unsigned char>(color.alpha());
       scene->setBackgroundColor(cColor);
-      if (pipeline)
+      if(pipeline)
         pipeline->setBackgroundColor(cColor);
       if (glWidget)
         glWidget->update();
@@ -1843,7 +1843,7 @@ void MainWindow::setRenderingSettings()
     dialog.exec();
     QSettings settings;
     settings.setValue("MainWindow/ao_enabled", pipeline->getAoEnabled());
-    settings.setValue("MainWindow/fog_enabled", pipeline->getFogEnabled());
+    settings.setValue("MainWindow/fog_enabled", pipeline->getFogEnabled());    
     settings.setValue("MainWindow/ao_strength", pipeline->getAoStrength());
     settings.setValue("MainWindow/ed_enabled", pipeline->getEdEnabled());
   }
@@ -2356,12 +2356,10 @@ void MainWindow::registerMoleQueue()
   StringList exts = ffm.fileExtensions(FileFormat::Read | FileFormat::File);
 
   // Create patterns list
-  QList<QRegularExpression> patterns;
+  QList<QRegExp> patterns;
   for (auto it = exts.begin(), itEnd = exts.end(); it != itEnd; ++it) {
-    patterns << QRegularExpression(
-      QRegularExpression::wildcardToRegularExpression(
-        extensionToWildCard(QString::fromStdString(*it))),
-      QRegularExpression::CaseInsensitive);
+    patterns << QRegExp(extensionToWildCard(QString::fromStdString(*it)),
+                        Qt::CaseInsensitive, QRegExp::Wildcard);
   }
 
   // Register the executable:
